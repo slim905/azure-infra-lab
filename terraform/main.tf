1955fa1f5c5234cdac019952d085f0e47d52c05c
slim905 <aniron@live.ca>
2025-10-25 19:16:34 -0400

    Adding 2nd resource group using variable

---
+++ terraform/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+
+  required_version = ">= 1.8.0"
+}
+
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+}
+
+
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-terraform-lab"
+  location = "West US"
+}
+
