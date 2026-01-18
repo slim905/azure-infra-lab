35fe017b0a86db4fc64cb19d910755af7e2b81bd
slim905 <aniron@live.ca>
2026-01-18 17:15:37 -0500

    Finalized outputs and Bastion removal

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,24 +1,26 @@
-output "main_rg_name" {
-  description = "Name of the main resource group"
+output "resource_group_name" {
+  description = "Name of the resource group"
   value       = azurerm_resource_group.rg-1.name
 }
 
-output "secondary_rg_name" {
-  description = "Name of the secondary resource group"
-  value       = azurerm_resource_group.rg-2.name
-}
-
 output "vnet_name" {
-  value = azurerm_virtual_network.main_vnet.name
-}
-
-output "storage_account_name" {
-  value = azurerm_storage_account.stg-2.name
+  description = "Virtual network name"
+  value       = azurerm_virtual_network.main_vnet.name
 }
 
 output "subnet_ids" {
+  description = "Subnet IDs by function"
   value = {
     app = azurerm_subnet.app_subnet.id
     db  = azurerm_subnet.db_subnet.id
   }
+}
+
+output "storage_account_name" {
+  description = "Storage account name"
+  value       = azurerm_storage_account.stg-2.name
+}
+
+output "db_subnet_id" {
+  value = azurerm_subnet.db_subnet.id
 }
